06a1409b511c724782c1d7590d0293b5eb44cd8a
Zwanan-github <270603801@qq.com>
2023-07-04 22:57:58 +0800

    feat(Front-end/component/settings/InfoSetting):编写前端请求的接口，和字段长度限制，在表单对象新增uid字段，用于获取用户id

--- frontend/src/components/settings/InfoSetting.vue
+++ frontend/src/components/settings/InfoSetting.vue
@@ -8,28 +8,28 @@
         style="max-width: 460px;"
     >
       <el-form-item label="用户名">
-        <el-input v-model="infoForm.username"/>
+        <el-input :maxlength="20" v-model="infoForm.username"/>
       </el-form-item>
       <el-form-item label="描述">
-        <el-input type="textarea" rows="4" placeholder="说说你自己吧" v-model="infoForm.desc"/>
+        <el-input :maxlength="500" type="textarea" rows="4" placeholder="说说你自己吧" v-model="infoForm.desc"/>
       </el-form-item>
       <el-form-item label="性别">
         <el-radio-group v-model="infoForm.sex">
-          <el-radio label="男" size="large">男</el-radio>
-          <el-radio label="女" size="large">女</el-radio>
+          <el-radio label="male" size="large">男</el-radio>
+          <el-radio label="female" size="large">女</el-radio>
         </el-radio-group>
       </el-form-item>
       <el-form-item label="手机号">
-        <el-input v-model="infoForm.phone"/>
+        <el-input :minlength="11" :maxlength="11" v-model="infoForm.phone"/>
       </el-form-item>
       <el-form-item label="QQ">
-        <el-input v-model="infoForm.qq"/>
+        <el-input :maxlength="12" v-model="infoForm.qq"/>
       </el-form-item>
       <el-form-item label="博客">
-        <el-input v-model="infoForm.blog"/>
+        <el-input :maxlength="50" v-model="infoForm.blog"/>
       </el-form-item>
     </el-form>
-    <el-button type="success" :icon="Select">保存个人信息</el-button>
+    <el-button type="success" :icon="Select" @click="saveInfo">保存个人信息</el-button>
   </div>
 </template>
 
@@ -37,8 +37,14 @@
 import {reactive} from "vue";
 
 import {Select} from "@element-plus/icons-vue"
+import {post} from "@/js";
+import {ElMessage} from "element-plus";
+import {useStore} from "@/stores/counter";
+
+const store = useStore();
 
 const infoForm = reactive({
+  uid: store.auth.user.id,
   username: '',
   desc: '',
   qq: '',
@@ -46,6 +52,14 @@ const infoForm = reactive({
   sex: '',
   phone: ''
 });
+
+const saveInfo = () => {
+  post('api/user/save-info', infoForm,()=>{
+    ElMessage.success("保存成功");
+  }, "json")
+};
+
+
 </script>
 
 <style scoped>
